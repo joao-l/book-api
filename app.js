571c6bf7a35fda86406ba9596b1d7ca656d22f6c
unknown <shkk27@gmail.com>
2017-09-20 22:27:44 -0300

    Add eslint config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import config from './config/config';
 import datasource from './config/datasource';
-import bodyParser from 'body-parser';
 
 const app = express();
 
